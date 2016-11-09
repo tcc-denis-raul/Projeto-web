497d399a155553049e934b4745e77fa8af4a7318
Denis Aoki <dnsaoki@gmail.com>
2016-11-09 20:04:29 -0200

    ..

--- app/cache.py
+++ app/cache.py
@@ -1,28 +1,15 @@
-import redis
 import json
-from urlparse import urlparse
 
 from app import settings
-
+from app.models import CacheModel
 
 class Cache():
-    def __init__(self):
-        self.host = urlparse(settings.REDIS_HOST)
-        self.client = redis.StrictRedis(
-            host=self.host.hostname,
-            port=int(self.host.port),
-            password=settings.REDIS_PWD
-        )
-        self.HASH = "courses"
-
     def _save(self, key, value):
         try:
-            if self.client.hexists(self.HASH, key):
-                self.client.expire(self.HASH, 300)
-            print "key: %s" % key
-            print "value: %s" % json.dumps(value)
-            self.client.hset(self.HASH, key, json.dumps(value))
-            self.client.expire(self.HASH, 300)
+            if CacheModel.objects.filter(name=value).exists():
+                CacheModel.objects.exclude(name=value)
+            value = CacheModel.objects.create(name=value, data=json.dumps(value))
+            value.save()
         except Exception as error:
             print "Dont save because: {}, {}, {}".format(
                 error, key, value
@@ -34,9 +21,6 @@ class Cache():
 
     def get(self, key):
         try:
-            if not self.client.hexists(self.HASH, key):
-                raise KeyError('invalid')
             value = self.client.hget(self.HASH, key)
-            return json.loads(value)
         except Exception:
             raise Exception
--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class CacheModel(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    data = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.data
